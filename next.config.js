7e9f3445bdbc3703a047bb123d3fb30f0f50f040
themartincox <martin@postino.cc>
2025-06-23 14:56:36 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ const nextConfig = {
 
   // Optimize images
   images: {
+    unoptimized: true, // Required for static export
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
